2ea8b5cf8d8fa3c01e685d6c33dba826107b0a5c
urmil89 <urmil8989@gmail.com>
2020-05-27 13:47:05 +0530

    changes in index file

--- index.php
+++ index.php
@@ -1,121 +1,134 @@
-<?php include 'header.php'; ?>
-<?php require 'config.php'; ?>
-<?php
-$title = '';
-$disc = '';
-$row = '';
-// calling functions
-inotes();
-idelete();
-iupdate();
+<?php 
+session_start();
+include 'header.php'; 
+require 'config.php'; 
 
-function inotes()
+if(isset($_SESSION['ilogin']))
 {
-    global $title;
-    global $conn;
-    global $disc;
-    if (isset($_POST['save'])) {
-        $title = $_POST['title'];
-        $disc = $_POST['description'];
-        if (!$title == '' || !$disc == '') {
-            $query = "INSERT INTO `notes` (`id`, `title`, `description`, `tstamp`) VALUES (NULL, '$title', '$disc', current_timestamp());";
+
+    
+    
+    
+    
+    
+    $title = '';
+    $disc = '';
+    $row = '';
+    // calling functions
+    inotes();
+    idelete();
+    iupdate();
+    
+    function inotes()
+    {
+        global $title;
+        global $conn;
+        global $disc;
+        if (isset($_POST['save'])) {
+            $title = $_POST['title'];
+            $disc = $_POST['description'];
+            if (!$title == '' || !$disc == '') {
+                $query = "INSERT INTO `notes` (`id`, `title`, `description`, `tstamp`) VALUES (NULL, '$title', '$disc', current_timestamp());";
+                $result = mysqli_query($conn, $query);
+                if ($result) {
+                    header('location:index.php');
+                }
+            }
+        }
+    }
+    
+    
+    function idelete()
+    {
+        
+        global $conn;
+        if (isset($_GET['did'])) {
+            
+            $id = $_GET['did'];
+            $query = "DELETE FROM `notes` WHERE `notes`.`id` = '$id'";
             $result = mysqli_query($conn, $query);
             if ($result) {
                 header('location:index.php');
             }
         }
     }
-}
-
-
-function idelete()
-{
-
-    global $conn;
-    if (isset($_GET['did'])) {
-
-        $id = $_GET['did'];
-        $query = "DELETE FROM `notes` WHERE `notes`.`id` = '$id'";
-        $result = mysqli_query($conn, $query);
-        if ($result) {
-            header('location:index.php');
+    
+    
+    function iupdate()
+    {
+        
+        global $id;
+        global $title;
+        global $conn;
+        global $disc;
+        
+        if (isset($_POST['update'])) {
+            
+            $id = $_POST['id'];
+            $title = $_POST['title'];
+            $disc = $_POST['description'];
+            $query = "UPDATE `notes` SET `title`='$title',`description`='$disc',`tstamp`=current_timestamp() WHERE id = '$id'";
+            $result = mysqli_query($conn, $query);
+            if ($result) {
+                header('location:index.php');
+            }
         }
     }
-}
-
-
-function iupdate()
-{
-
-    global $id;
-    global $title;
-    global $conn;
-    global $disc;
-
-    if (isset($_POST['update'])) {
-
-        $query = "UPDATE `notes` SET `title`='$title',`description`='$disc',`tstamp`='current_timestamp()' WHERE 1";
-        $result = mysqli_query($conn, $query);
-        if ($result) {
-            header('location:index.php');
-        }
-    }
-}
-
-?>
-
-<div class="container my-4">
-    <h2>Add Your Note</h2>
-    <form action="index.php" method="post">
-        <div class="form-group">
-            <label for="title">Note title</label>
-            <input type="text" value="<?php echo $row['title'] ?>" class="form-control" id="title" name="title" aria-describedby="emailHelp" required>
-        </div>
-        <div class="form-group">
-            <label for="desc">Note Description</label>
-            <textarea class="form-control" name="description" id="desc" rows="3" required><?php echo $row['description']; ?></textarea>
-        </div>
-        <button type="submit" name="save" class="btn btn-primary">Add Note</button>
-    </form>
-</div>
+    
+    ?>
 
 <?php
 
 if (isset($_GET['eid'])) {
-
+    
     $id = $_GET['eid'];
     $query = "SELECT * FROM `notes` WHERE id = '$id'";
     $result = mysqli_query($conn, $query);
     $row = mysqli_fetch_array($result);
     $_SESSION['update'] = true;
-?>
+    ?>
     <div class="container my-4">
         <h2>Add Your Note</h2>
         <form action="index.php" method="post">
             <div class="form-group">
                 <label for="title">Note title</label>
+                <input type="hidden" name="id" value="<?php echo $row['id']; ?>">
                 <input type="text" value="<?php echo $row['title'] ?>" class="form-control" id="title" name="title" aria-describedby="emailHelp" required>
             </div>
             <div class="form-group">
                 <label for="desc">Note Description</label>
                 <textarea class="form-control" name="description" id="desc" rows="3" required><?php echo $row['description']; ?></textarea>
             </div>
-            <?php
-            if (isset($_SESSION['update'])) {
-            ?>
-                <button type="submit" name="update" class="btn btn-warning">Update Note</button>
-            <?php
-            } else {
-            ?>
-                <button type="submit" name="save" class="btn btn-primary">Add Note</button>
-            <?php
-            }
-            ?>
+            <button type="submit" name="update" class="btn btn-warning">Update Note</button>
         </form>
-    </div <?php
-        }
-            ?> <div class="container my-4">
+    </div>
+    <?php
+} else {
+    ?>
 
+<div class="container my-4">
+        <h2>Add Your Note</h2>
+        <form action="index.php" method="post">
+            <div class="form-group">
+                <label for="title">Note title</label>
+                <input type="text" class="form-control" id="title" name="title" aria-describedby="emailHelp" required>
+            </div>
+            <div class="form-group">
+                <label for="desc">Note Description</label>
+                <textarea class="form-control" name="description" id="desc" rows="3" required></textarea>
+            </div>
+            <button type="submit" name="save" class="btn btn-primary">Add Note</button>
+        </form>
+    </div>
+    
+    <?php
+}
+
+?>
+
+
+<div class="container my-4">
+    
     <table class="table" id="myTable">
         <thead>
             <tr>
@@ -133,7 +146,7 @@ if (isset($_GET['eid'])) {
             $sno = 0;
             while ($row = mysqli_fetch_assoc($result)) {
                 $sno = $sno + 1;
-            ?>
+                ?>
                 <tr>
                     <th scope='row'><?php echo $sno; ?></th>
                     <td><?php echo $row['title'] ?></td>
@@ -142,13 +155,22 @@ if (isset($_GET['eid'])) {
                     <td><a href="index.php?eid=<?php echo $row['id']; ?>"><button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModal">Edit</button></a> <a href="index.php?did=<?php echo $row['id']; ?>"><button type="button" class="btn btn-danger">Delete</button></a></td>
                 <?php
             }
-                ?>
+            ?>
         </tbody>
     </table>
+    
+    
+    
+</div>
+<hr>
+<?php
+}
+else{
 
 
 
-    </div>
-    <hr>
 
-    <?php include 'footer.php'; ?>
+    
+}
+?>
+<?php include 'footer.php'; ?>
--- signup.php
+++ signup.php
@@ -1,6 +1,7 @@
 <?php
 session_start();
-include 'config.php';
+require 'config.php';
+include 'header.php';
 
 if (isset($_POST['submit'])) {
     $username = $_POST['username'];
@@ -18,6 +19,7 @@ if (isset($_POST['submit'])) {
             if ($result) {
                 header('location:index.php');
             } else {
+
                 header('location:signup.php');
             }
 
@@ -37,105 +39,145 @@ if (isset($_POST['submit'])) {
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-<?php include 'header.php'; ?>
-
-
 <div class="container text-center mt-5">
-
-    <!-- Signup Form -->
-    <form class="needs-validation" novalidate method="post" action="signup.php">
-        <div class="form-row">
-            <div class="col-md-12 mb-3">
-                <label for="validationCustomUsername">Username</label>
-                <div class="input-group">
-                    <div class="input-group-prepend">
-                        <span class="input-group-text" id="inputGroupPrepend">@</span>
-                    </div>
-                    <input type="text" name="username" class="form-control" id="validationCustomUsername" aria-describedby="inputGroupPrepend" required>
-                    <div class="invalid-feedback">
-                        Please choose a username.
-                    </div>
-                </div>
-            </div>
-        </div>
-        <div class="form-row">
-            <div class="col-md-12 mb-3">
-                <label for="validationCustomUsername">Password</label>
-                <div class="input-group">
-                    <div class="input-group-prepend">
-                        <span class="input-group-text" id="inputpassword">#</span>
-                    </div>
-                    <input type="password" name="password" class="form-control" id="validationCustomPassword" aria-describedby="inputpassword" required>
-                    <div class="invalid-feedback">
-                        Please choose a password.
+    <div class="row">
+        <div class="col-md-6">
+            <!-- Signup Form -->
+            <h1>SignUp</h1>
+            <form class="needs-validation" novalidate method="post" action="index.php">
+                <div class="form-row">
+                    <div class="col-md-12 mb-3">
+                        <label for="validationCustomUsername">Username</label>
+                        <div class="input-group">
+                            <div class="input-group-prepend">
+                                <span class="input-group-text" id="inputGroupPrepend">@</span>
+                            </div>
+                            <input type="text" name="username" class="form-control" id="validationCustomUsername" aria-describedby="inputGroupPrepend" required>
+                            <div class="invalid-feedback">
+                                Please choose a username.
+                            </div>
+                        </div>
                     </div>
                 </div>
-            </div>
+                <div class="form-row">
+                    <div class="col-md-12 mb-3">
+                        <label for="validationCustomUsername">Password</label>
+                        <div class="input-group">
+                            <div class="input-group-prepend">
+                                <span class="input-group-text" id="inputpassword">#</span>
+                            </div>
+                            <input type="password" name="password" class="form-control" id="validationCustomPassword" aria-describedby="inputpassword" required>
+                            <div class="invalid-feedback">
+                                Please choose a password.
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="form-group">
+                    <div class="col-md-12 mb-3">
+                        <div class="form-check custom-control custom-checkbox form-check">
+                            <input class="form-check-input custom-control-input" type="checkbox" id="invalidCheck" required>
+                            <label class="custom-control-label" for="invalidCheck">
+                                Agree to terms and conditions
+                            </label>
+                        </div>
+                        <div class="invalid-feedback">
+                            You must agree before submitting.
+                        </div>
+                    </div>
+                    <button class="btn btn-primary" name="submit" type="submit">Submit</button>
+                    <div>
+                    </div>
+            </form>
+
+        </div>
         </div>
 
-        <div class="form-group">
-            <div class="form-check custom-control custom-checkbox form-check">
-                <input class="form-check-input custom-control-input" type="checkbox" id="invalidCheck" required>
-                <label class="custom-control-label" for="invalidCheck">
-                    Agree to terms and conditions
-                </label>
+
+
+        <!-- Login Form -->
+            <div class="col-md-6">
+                <h1>Login</h1>
+                <form class="needs-validation" novalidate method="post" action="index.php">
+                    <div class="form-row">
+                        <div class="col-md-12 mb-3">
+                            <label for="validationCustomUsername">Username</label>
+                            <div class="input-group">
+                                <div class="input-group-prepend">
+                                    <span class="input-group-text" id="inputGroupPrepend">@</span>
+                                </div>
+                                <input type="text" name="username" class="form-control" id="validationCustomUsername" aria-describedby="inputGroupPrepend" required>
+                                <div class="invalid-feedback">
+                                    Please choose a username.
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="form-row">
+                        <div class="col-md-12 mb-3">
+                            <label for="validationCustomUsername">Password</label>
+                            <div class="input-group">
+                                <div class="input-group-prepend">
+                                    <span class="input-group-text" id="inputpassword">#</span>
+                                </div>
+                                <input type="password" name="password" class="form-control" id="validationCustomPassword" aria-describedby="inputpassword" required>
+                                <div class="invalid-feedback">
+                                    Please choose a password.
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="form-group">
+                        <div class="col-md-12 mb-3">
+                            <div class="form-check custom-control custom-checkbox form-check">
+                                <input class="form-check-input custom-control-input" type="checkbox" id="invalidCheck" required>
+                                <label class="custom-control-label" for="invalidCheck">
+                                    Agree to terms and conditions
+                                </label>
+                            </div>
+                            <div class="invalid-feedback">
+                                You must agree before submitting.
+                            </div>
+                        </div>
+                        <button class="btn btn-primary" name="submit" type="submit">Submit</button>
+                        <div>
+                        </div>
+                </form>
             </div>
-            <div class="invalid-feedback">
-                You must agree before submitting.
-            </div>
-        </div>
-        <button class="btn btn-primary" name="submit" type="submit">Submit</button>
+    </div>
 </div>
-</form>
 
-<script>
-    // Example starter JavaScript for disabling form submissions if there are invalid fields
-    (function() {
-        'use strict';
-        window.addEventListener('load', function() {
-            // Fetch all the forms we want to apply custom Bootstrap validation styles to
-            var forms = document.getElementsByClassName('needs-validation');
-            // Loop over them and prevent submission
-            var validation = Array.prototype.filter.call(forms, function(form) {
-                form.addEventListener('submit', function(event) {
-                    if (form.checkValidity() === false) {
-                        event.preventDefault();
-                        event.stopPropagation();
-                    }
-                    form.classList.add('was-validated');
+
+
+
+
+
+        <script>
+            // Example starter JavaScript for disabling form submissions if there are invalid fields
+            (function() {
+                'use strict';
+                window.addEventListener('load', function() {
+                    // Fetch all the forms we want to apply custom Bootstrap validation styles to
+                    var forms = document.getElementsByClassName('needs-validation');
+                    // Loop over them and prevent submission
+                    var validation = Array.prototype.filter.call(forms, function(form) {
+                        form.addEventListener('submit', function(event) {
+                            if (form.checkValidity() === false) {
+                                event.preventDefault();
+                                event.stopPropagation();
+                            }
+                            form.classList.add('was-validated');
+                        }, false);
+                    });
                 }, false);
-            });
-        }, false);
-    })();
-</script>
+            })();
+        </script>
 
 
-</div>
+    </div>
 
 
 
-<?php include 'footer.php'; ?>
+    <?php include 'footer.php'; ?>
