d8f3820b58bfbbad6c30a416a48e751961bd5f81
urmil89 <urmil8989@gmail.com>
2020-05-28 21:52:47 +0530

    change on header

--- header.php
+++ header.php
@@ -26,24 +26,7 @@ session_start();
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <ul class="navbar-nav mr-auto">
                 <li class="nav-item active">
-                    <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
-                </li>
-                <li class="nav-item dropdown">
-                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                        Advance
-                    </a>
-                    <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                        <a class="dropdown-item" href="#">GO to Premium</a>
-                        <a class="dropdown-item" href="#">Cool Features</a>
-                        <div class="dropdown-divider"></div>
-                        <a class="dropdown-item" href="#">Our Projects</a>
-                    </div>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">About</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">contact</a>
+                    <a class="nav-link" href="#">Keep your notes <span class="sr-only">(current)</span></a>
                 </li>
             </ul>
             <ul class="navbar-nav">
